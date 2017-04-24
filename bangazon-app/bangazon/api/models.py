eb3320dc26d4074728b64f1584f4bcddaac44a85
Harper Frankstone <Harper Frankstone>
2017-04-24 13:57:15 -0500

    wrote the table for the training programs and also changed the order of the classes in the models.py

--- bangazon-app/bangazon/api/models.py
+++ bangazon-app/bangazon/api/models.py
@@ -9,32 +9,47 @@ class ProductType(models.Model):
 	'''
 	producttypename = models.CharField(max_length=100)
 
+class Customer(models.Model):
+    """ 
+    author: Kayla Brewer
+    purpose: defines properties associated with the customer object
+    properties: customerFirstName, customerLastName, active, customerSince
+    """
+    customerFirstName = models.CharField(max_length=100)
+    customerLastName = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    customerSince = models.DateField(auto_now_add=True)
 
 
 #employee payment type:
 class CustomerPaymentType(models.Model):
-	'''	author: Casey Dailey
-		purpose: 
-		defines properties associated with the customers payment type
-	   	properties:
-		paymentTypeAccountNumber=integer, associated with whatever payment method account used
-		paymentTypeName=string, name of given payment type (credit, debit, gift card)
-		customerID=integer, foreign key from customer table establishes the relationship between customer and payment type
-	'''
-	paymentTypeAccountNumber = models.IntegerField()
-	paymentTypeName = models.CharField(max_length=50)
-	#customerID = models.ForeignKey(Customer)
-
-
+    ''' author: Casey Dailey
+        purpose: 
+        defines properties associated with the customers payment type
+        properties:
+        paymentTypeAccountNumber=integer, associated with whatever payment method account used
+        paymentTypeName=string, name of given payment type (credit, debit, gift card)
+        customerID=integer, foreign key from customer table establishes the relationship between customer and payment type
+    '''
+    paymentTypeAccountNumber = models.IntegerField()
+    paymentTypeName = models.CharField(max_length=50)
+    customerID = models.ForeignKey(Customer)
 
 class Orders(models.Model):
-	# customerId = models.ForeignKey(Customer)
-	# paymentTypeId = models.ForeignKey(PaymentType)
-	pass 
+    '''author: Harper Franktone
+       purpose: defines properties associated with the Orders
+       properties: customerId = relates the orders table to the Customer table through a Foreign Key, paymentTypeId = relates the orders table to the PaymentType table through a foreign key
+    '''
+    customerId = models.ForeignKey(Customer)
+    paymentTypeId = models.ForeignKey(CustomerPaymentType)
 
 class Department(models.Model):
-	departmentName = models.CharField(max_length=100)
-	departmentBudget = models.IntegerField()
+    '''author: Harper Franktone
+       purpose: defines properties associated with the Departments
+       properties: departmentName = builds the department name column in the department table, departmentBudget = builds the department budget column for the the department table 
+    '''
+    departmentName = models.CharField(max_length=100)
+    departmentBudget = models.IntegerField()
 
 class EmployeeType(models.Model):
 	'''
@@ -44,7 +59,7 @@ class EmployeeType(models.Model):
 	and columns: employeeFirstName, employeeLastName, employeeTitle, employeePay
 	and supervisor.
 	'''
-#	departmentID = models.ForeignKey(Department)
+	departmentID = models.ForeignKey(Department)
 	employeeFirstName = models.CharField(max_length=100)
 	employeeLastName = models.CharField(max_length=100)
 	employeeTitle = models.CharField(max_length=100)
@@ -74,20 +89,19 @@ class Product(models.Model):
 	productDescription=string, associated with the specific product's description
 	docs: http://www.django-rest-framework.org/api-guide/serializers/#serializer-inheritance 
 	'''
-	# productTypeID = models.ForeignKey(ProductType)
-	# customerID = models.ForeignKey(Customer)
+	productTypeID = models.ForeignKey(ProductType)
+	customerID = models.ForeignKey(Customer)
 	productName = models.CharField(max_length=100)
 	productPrice = models.CharField(max_length=25)
 	productDescription = models.CharField(max_length=100)
 
-class Customer(models.Model):
-    """	
-    author: Kayla Brewer
-    purpose: defines properties associated with the customer object
-    properties: customerFirstName, customerLastName, active, customerSince
-	"""
-    customerFirstName = models.CharField(max_length=100)
-    customerLastName = models.CharField(max_length=100)
-    active = models.BooleanField(default=True)
-    customerSince = models.DateField(auto_now_add=True)
 
+class TrainingPrograms(models.Model):
+    '''author: Harper Franktone
+       purpose: defines properties associated with the TrainingProgram
+       properties: 
+    '''
+    trainingProgramName = models.CharField(max_length=100)
+    startDate = models.CharField(max_length=100)
+    endDate = models.CharField(max_length=100)
+    maxAttendees = models.IntegerField()
