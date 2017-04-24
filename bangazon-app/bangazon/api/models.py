f0e151fc200dd421a7762ea059f9f42357d2f871
Jordan Nelson <jnelsontn@gmail.com>
2017-04-24 09:39:01 -0500

    completed employee table

--- bangazon-app/bangazon/api/models.py
+++ bangazon-app/bangazon/api/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class ProductType(models.Model):
 	producttypename = models.CharField(max_length=100)
+
+class EmployeeType(models.Model):
+#	departmentID = models.ForeignKey(Department)
+	employeeFirstName = models.CharField(max_length=100)
+	employeeLastName = models.CharField(max_length=100)
+	employeeTitle = models.CharField(max_length=100)
+	employeePay = models.CharField(max_length=100)
+	supervisor = models.CharField(max_length=100)
